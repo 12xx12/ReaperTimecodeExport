007ab6d003f3fc59ecba650e5e42d25ab1fb7dc4
12xx12 <12xx12100@gmail.com>
2020-08-08 15:48:20 +0200

    trying to make the setup file work

--- setup.py
+++ setup.py
@@ -3,10 +3,11 @@ from setuptools import setup
 setup(
     name='ReaperTimecodeExport',
     version='1.0',
-    packages=['lxml'],
+    packages=[''],
     url='https://github.com/12xx12/ReaperTimecodeExport',
     license='GLP-3.0',
     author='12xx12',
     author_email='12xx12100@gmail.com',
-    description=''
+    description='',
+    install_requires='lxml'
 )
